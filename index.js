b0cab8226f02b3ba5819c8810080965d916b9572
vijayadhwaja <vijayadhwaja@digital-result.com>
2023-06-30 15:44:53 +0200

    adding build and test files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Welcome to lambda CI/CD demo!!')
+  res.status(200).send('Welcome to lambda CI/CD demo!!')
 })
 
 app.listen(3000)
